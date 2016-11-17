c64138874d6a9e3cc7659919f95c6137df48ef9e
rajeevnagarwal <rajeev14084@iiitd.ac.in>
2016-11-17 14:08:46 +0530

    Added Two classes

---
+++ app/src/main/java/com/example/himaneeshchhabra/loginproject/Message.java
@@ -0,0 +1,40 @@
+package com.example.himaneeshchhabra.loginproject;
+
+import java.util.Date;
+
+/**
+ * Created by Rajeev Nagarwal on 11/17/2016.
+ */
+
+public class Message {
+    private String message;
+    private String sender;
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+
+}
